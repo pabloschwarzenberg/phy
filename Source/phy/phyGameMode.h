7c89dc76f8fdfe8a9a796bfb2adfe7f70026f2c9
Pablo Schwarzenberg <pabloschwarzenberg@alcyone.local>
2022-08-25 11:55:58 -0400

    base

---
+++ Source/phy/phyGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "phyGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AphyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AphyGameMode();
+};
+
+
+
